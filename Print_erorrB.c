c66ead1fccc738da6892119bdee367fc396370ce
mnalrefaey <m.n.alrefaey@gmail.com>
2023-08-18 15:38:37 -0700

    rr

--- Print_erorrB.c
+++ Print_erorrB.c
@@ -2,6 +2,7 @@
 
 /**
  * _epuuts - string input to print
+ *
  * @strg: print the string
  * Return: Non.
 */
@@ -10,75 +11,70 @@ void _epuuts(char *strg)
 {
 int a = 0;
 
-if (!strg)
+if (strg == NULL)
 return;
-
 while (strg[a] != '\0')
 {
 _epuutchar(strg[a]);
-
 a++;
-
 }
 }
 
 /**
  * _epuutchar - print the character z to the stderr
+ *
  * @z: The printed character
+ *
  * Return: success always 1, otherwise -1
 */
 
 int _epuutchar(char z)
 {
-static int z;
-
+static int x;
 static char buff[WR_BUFF_SIZE];
 
-if (z == BUFF_FLSH || a >= WRITE_BUFF_SIZE)
+if (z == BUFF_FLSH || x >= WR_BUFF_SIZE)
 {
-write(2, buf, a);
-
-a = 0;
-
+write(2, buff, x);
+x = 0;
 }
 if (z != BUFF_FLSH)
-buf[a++] = z;
-
+buff[x++] = z;
 return (1);
-
 }
 
 /**
  * _putf - the character z written to give f
+ *
  * @z: the printed character
+ *
  * @f: file to write script
+ *
  * Return: success always 1, otherwise -1
 */
 
 int _putf(char z, int f)
 {
 static int a;
+static char buff[WR_BUFF_SIZE];
 
-static char buff[WRITE_BUFF_SIZE];
-
-if (z == BUFF_FLSH || a >= WRITE_BUFF_SIZE)
+if (z == BUFF_FLSH || a >= WR_BUFF_SIZE)
 {
 write(f, buff, a);
-
 a = 0;
-
 }
 if (z != BUFF_FLSH)
 buff[a++] = z;
-
 return (1);
-
 }
 
 /**
  * _putsf - string input to print
+ *
  * @strg: print the string
+ *
  * @f: file to write script
+ *
  * Return: number of put chars
 */
 
@@ -86,14 +82,11 @@ int _putsf(char *strg, int f)
 {
 int a = 0;
 
-if (!strg)
+if (strg == NULL)
 return (0);
-
 while (*strg)
 {
 a += _putf(*strg++, f);
-
 }
 return (a);
-
 }
